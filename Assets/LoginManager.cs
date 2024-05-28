2962966e1978e345548906f124d617455651eb7a
Julibarrera2 <164010054+Julibarrera2@users.noreply.github.com>
2024-05-28 09:47:02 -0300

    completado

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    //Cuando el usuario presione el btn aceptar
+    //se debe escribir el mensaje en la consola
+    //Si el texto de input password conicide con
+    //la contraseña predeterminada escribir "access garanted"
+    //sino escribir "access denied"    
+
+    public InputField inputnombre; 
+    public string contraseña;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void contra()
+    {
+        if (inputnombre.text == contraseña)
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+
+    }
+}
